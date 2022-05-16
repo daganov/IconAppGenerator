06f89dc3f59df99e32186b53f6f5a9b05fdae685
Denis Aganov <den.aganov@yandex.ru>
2022-05-16 12:28:40 +0300

    Initial Commit

---
+++ IconAppGenerator/IconAppGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconAppGeneratorApp.swift
+//  IconAppGenerator
+//
+//  Created by Denis Aganov on 16.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct IconAppGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
